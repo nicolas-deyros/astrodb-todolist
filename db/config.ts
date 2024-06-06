caea92a8dade82cc01c53c7d2aa4376f2dc2e0a6
Nicolas Deyros <ndeyros@Lenovo.com>
2024-06-06 17:39:46 -0300

    feat: resolve db store with Astro.request.method

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,15 @@
-import { defineDb } from "astro:db";
+import { defineDb, defineTable, column, NOW } from "astro:db";
+
+const Todo = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true, unique: true }),
+    content: column.text(),
+    published: column.date({ default: NOW }),
+    status: column.json(),
+  },
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {},
+  tables: { Todo },
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,16 @@
-import { db } from "astro:db";
+import { db, Todo } from "astro:db";
 
 // https://astro.build/db/seed
 export default async function seed() {
-  // TODO
+  await db.insert(Todo).values([
+    { id: 1, content: "Coding", status: { value: "todo" } },
+    { id: 2, content: "Prototype", status: { value: "todo" } },
+    { id: 3, content: "User Testing", status: { value: "in progress" } },
+    { id: 4, content: "Analysis", status: { value: "in progress" } },
+    { id: 5, content: "Ideation", status: { value: "done" } },
+    { id: 6, content: "Sketches", status: { value: "done" } },
+    { id: 7, content: "Hypotesis", status: { value: "done" } },
+    { id: 8, content: "Interviews", status: { value: "trash" } },
+    { id: 9, content: "Research", status: { value: "trash" } },
+  ]);
 }
